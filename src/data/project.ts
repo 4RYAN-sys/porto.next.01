60a7a99a5b87ea04129642ebc4eb3bb8e6fab195
4RYAN-sys <susantoaryan488@gmail.com>
2025-09-04 11:23:34 +0700

    halah

--- src/data/project.ts
+++ src/data/project.ts
@@ -16,7 +16,7 @@ export const projects: Project[] = [
     description: "Portofolio pertama yang sederhana  ",
     github: "https://github.com/4RYAN-sys/hahahaha.git",
     demo: "https://slow-bro.vercel.app/",
-    image: "/project-images/project1.jpg",
+    image: "/project-image/project1.jpg",
     tech: ["Javascript", "HTML", "CSS"]
   },
   {
@@ -25,7 +25,7 @@ export const projects: Project[] = [
     description: "Web sederhana untuk menampilkan biodata",
     github: "https://github.com/4RYAN-sys/New.git",
     demo: "https://new-seven-oatt.vercel.app/",
-    image: "/project-images/project2.jpg",
+    image: "/project-image/project2.jpg",
     tech: ["HTML", "CSS",]
   },
   {
@@ -34,7 +34,7 @@ export const projects: Project[] = [
     description: "Project SPA react pertama",
     github: "https://github.com/4RYAN-sys/portofolio-react.git",
     demo: "https://portofolioaryan.netlify.app/",
-    image: "/project-images/project3.jpg",
+    image: "/project-image/project3.jpg",
     tech: ["React+vite", "JavaScript"]
   },
 ];
